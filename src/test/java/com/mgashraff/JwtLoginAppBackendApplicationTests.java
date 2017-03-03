741f237d747d79ae127a666107c40064d5033ca5
Mohammed Gulzar Ashraf <Mohammed Gulzar Ashraf>
2017-03-03 23:11:34 +0530

    Initial commit

---
+++ src/test/java/com/mgashraff/JwtLoginAppBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mgashraff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtLoginAppBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
